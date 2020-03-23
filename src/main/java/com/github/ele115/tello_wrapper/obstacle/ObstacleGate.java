cf46fd815a507fa6e663048d2fce136fc6a7b653
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-03-23 15:37:13 -0400

    gate rot

--- src/main/java/com/github/ele115/tello_wrapper/obstacle/ObstacleGate.java
+++ src/main/java/com/github/ele115/tello_wrapper/obstacle/ObstacleGate.java
@@ -6,6 +6,8 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 
 public class ObstacleGate implements IObstacle {
 
@@ -13,21 +15,24 @@ public class ObstacleGate implements IObstacle {
     private final static double GWIDTH = 120;
     private final static double BLOCKWIDTH = 20;
 
-    private final double x, y, z;
+    private final double x, y, z, angle;
     private final Group g;
     private final PhongMaterial m;
     private int state = 0;
 
-    public ObstacleGate(double x, double y, double z) {
+    public ObstacleGate(double x, double y, double z, double angle) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.angle = angle;
         this.g = new Group();
         this.m = new PhongMaterial();
         this.m.setDiffuseMap(new Image("/brick.jpg"));
         this.m.setSpecularColor(Color.DARKRED);
 
-        this.g.setTranslateY(-GHEIGHT / 2);
+        this.g.getTransforms().add(new Translate(y, -GHEIGHT / 2 - z, -x));
+        this.g.getTransforms().add(new Rotate(90 - angle, Rotate.Y_AXIS));
+
         {
             var o = new Box(BLOCKWIDTH, GHEIGHT, BLOCKWIDTH);
             o.setTranslateX(-GWIDTH / 2 + BLOCKWIDTH / 2);
@@ -55,10 +60,12 @@ public class ObstacleGate implements IObstacle {
     }
 
     public boolean contains(double x, double y, double z) {
-        return Math.abs(x - this.x) <= BLOCKWIDTH / 2
-                && Math.abs(y - this.y) <= GWIDTH / 2
+        var x0 = -Math.sin(Math.toRadians(angle)) * (x - this.x) + Math.cos(Math.toRadians(angle)) * (y - this.y);
+        var y0 = Math.cos(Math.toRadians(angle)) * (x - this.x) + Math.sin(Math.toRadians(angle)) * (y - this.y);
+        return Math.abs(x0) <= BLOCKWIDTH / 2
+                && Math.abs(y0) <= GWIDTH / 2
                 && Math.abs(z - GHEIGHT / 2 - this.z) <= GHEIGHT / 2
-                && !(Math.abs(y - this.y) < GWIDTH / 2 - BLOCKWIDTH
+                && !(Math.abs(y0) < GWIDTH / 2 - BLOCKWIDTH
                 && Math.abs(z - GHEIGHT / 2 - this.z) < GHEIGHT / 2 - BLOCKWIDTH);
     }
 
