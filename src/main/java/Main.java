aa4799a227da40f345c9e8df0b5ae83390bdaeaf
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-03-30 06:48:55 -0400

    better main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.github.ele115.tello_wrapper.ITelloDrone;
 import com.github.ele115.tello_wrapper.Tello;
 import com.github.ele115.tello_wrapper.obstacle.ObstacleGate;
 import com.github.ele115.tello_wrapper.tello4j.api.video.VideoWindow;
+import com.github.ele115.tello_wrapper.tello4j.api.world.FlipDirection;
 import javafx.scene.paint.Color;
 
 import java.awt.image.BufferedImage;
@@ -14,26 +15,43 @@ public class Main {
             System.exit(1);
         }
 
+        // You may reduce the size of all windows.
+        // However, your program is NOT guaranteed to work.
+        Tello.setWindowSize(960, 720);
+
+        // Add a gate to the scene.
+        Tello.getSimulator().addObstacle(new ObstacleGate(60, 250, 50, 20, Color.RED));
+        Tello.getSimulator().setNoisy(true);
+
+        // Uncomment the following lines if you don't have a good video card
+        // Tello.getSimulator().setSnapshotInt(300);
+        // Tello.getSimulator().setDelayUpdate(true);
+
         int droneX = Integer.parseInt(args[0]);
         int droneY = Integer.parseInt(args[1]);
         int droneZ = Integer.parseInt(args[2]);
 
-        Tello.setWindowSize(960, 720);
-        Tello.getSimulator().addObstacle(new ObstacleGate(60, 250, 50, 20, Color.RED));
-        Tello.getSimulator().setNoisy(true);
+        // The 90 (degrees) here means that the drone is facing forward.
         ITelloDrone drone = Tello.Connect("simulator", droneX, droneY, 90);
+
+        // You may opt-out for this window by commenting out this line.
         drone.addVideoListener(new VideoWindow());
+
+        // ... but you don't want to comment out this line of course
         FrameGrabber frameGrabber = new FrameGrabber(1);
         drone.addVideoListener(frameGrabber);
+
         drone.setStreaming(true);
         drone.takeoff();
-        drone.up(droneZ - 50);
+        drone.up(droneZ - 50); // Move to the desired location
 
-        // Fly through a gate
         alignGateAndFrameCenters(drone, frameGrabber);
-        drone.forward(-droneY + 400);
+
+        drone.forward(-droneY + 400); // Fly to the other end of the room
+        drone.flip(FlipDirection.FORWARD); // Celebrate
 
         drone.land();
+        System.exit(0);
     }
 
     public static void alignGateAndFrameCenters(ITelloDrone drone, FrameGrabber frameGrabber) {
